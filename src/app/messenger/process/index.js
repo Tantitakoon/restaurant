e25288cbb4b174c550b8b0868637760487b7121f
kilit <kilit130@infin.postbox.in.th>
2018-09-21 16:13:50 +0700

    A

--- src/app/messenger/process/index.js
+++ src/app/messenger/process/index.js
@@ -58,10 +58,10 @@ var process = exports.process = function (_Intent) {
         value: function onInit() {
            return this.use(this.res).end();
         }
-    }, ,{
-        key: 'onInitPlace',
-        value: function onInitPlace() {
-            serviceResponse.callGetPlace();
+    }, {
+        key: 'getLocation',
+        value: function getLocation() {
+                console.log('getLocation');
         }
     }, {
         key: 'res',
@@ -261,7 +261,7 @@ var process = exports.process = function (_Intent) {
 
                                     let serviceResponse =  new _services.responseMessenger(this.app);
                                    serviceResponse.callSendAPI('/messages',request_body, () => {});
-                                  
+                                   serviceResponse.callGetPlace();
                                                         
                         }
                               
