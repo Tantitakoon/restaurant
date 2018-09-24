60a4cbe8dd4636f04c4262c86b270ce556c8ea38
kilit <kilit130@infin.postbox.in.th>
2018-09-24 17:02:21 +0700

    A

--- src/app/messenger/process/index.js
+++ src/app/messenger/process/index.js
@@ -69,39 +69,70 @@ var process = exports.process = function (_Intent) {
                    
 
              let response = {
-                                    attachment: {
-                                        type: "template",
-                                        payload: {
-                                            template_type: "list",
-                                            top_element_style: "compact",
-                                            
-                                             elements: [
-                                             {
-                                                title: "Classic T-Shirt Collection",
-                                                subtitle: "See all our colors",
-                                               // image_url: "https://scontent.xx.fbcdn.net/v/t1.0-1/c1.0.50.50/p50x50/10645025_758268484219520_6811951924776374550_n.jpg?_nc_cat=101&oh=6be0c983f2f5307ccb82caea1204293d&oe=5C29187B",          
-                                                buttons: [
-                                                {
-                                                    title: "View",
-                                                    type: "web_url",
-                                                    url: "https://peterssendreceiveapp.ngrok.io/collection",
-                                                    messenger_extensions: true,
-                                                    webview_height_ratio: "tall",
-                                                    fallback_url: "https://peterssendreceiveapp.ngrok.io/"            
-                                                }
-                                                ]
-                                             }],
-                                            buttons: [{
-                                                type: "web_url",
-                                                url: "https://chatbotboom.herokuapp.com/messengerView",
-                                                title: "View More",
-                                                webview_height_ratio: "tall",
-                                                messenger_extensions: false
-                                            }]
+                                   "attachment": {
+                                    "type": "template",
+                                    "payload": {
+                                        "template_type": "list",
+                                        "top_element_style": "compact",
+                                        "elements": [
+                                        {
+                                            "title": "Classic T-Shirt Collection",
+                                            "subtitle": "See all our colors",
+                                            "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+                                            "buttons": [
+                                            {
+                                                "title": "View",
+                                                "type": "web_url",
+                                                "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                                                "messenger_extensions": true,
+                                                "webview_height_ratio": "tall",
+                                                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                                            }
+                                            ]
+                                        },
+                                        {
+                                            "title": "Classic White T-Shirt",
+                                            "subtitle": "See all our colors",
+                                            "default_action": {
+                                            "type": "web_url",
+                                            "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                                            "messenger_extensions": false,
+                                            "webview_height_ratio": "tall"
+                                            }
+                                        },
+                                        {
+                                            "title": "Classic Blue T-Shirt",
+                                            "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                                            "subtitle": "100% Cotton, 200% Comfortable",
+                                            "default_action": {
+                                            "type": "web_url",
+                                            "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                                            "messenger_extensions": true,
+                                            "webview_height_ratio": "tall",
+                                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                            },
+                                            "buttons": [
+                                            {
+                                                "title": "Shop Now",
+                                                "type": "web_url",
+                                                "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                                                "messenger_extensions": true,
+                                                "webview_height_ratio": "tall",
+                                                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                                            }
+                                            ]        
                                         }
+                                        ],
+                                        "buttons": [
+                                        {
+                                            "title": "View More",
+                                            "type": "postback",
+                                            "payload": "payload"            
+                                        }
+                                        ]  
                                     }
-                                };
-
+                                    }
+                                }
 
             //  var response =  {
                 
@@ -177,6 +208,7 @@ var process = exports.process = function (_Intent) {
                       "id": sender_psid
                      },
                       "message": response
+                      
                 };
                 serviceResponse.callSendAPI('/messages',request_body, () => {}); 
 
