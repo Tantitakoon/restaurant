31cd1ce471efeb328548436ad63be725ac353960
kilit <kilit130@infin.postbox.in.th>
2018-09-24 16:45:27 +0700

    A

--- src/app/messenger/process/index.js
+++ src/app/messenger/process/index.js
@@ -65,6 +65,8 @@ var process = exports.process = function (_Intent) {
              let serviceResponse =  new _services.responseMessenger(this.app);
              let nearbyPlace = await serviceResponse.callGetPlace();
              console.log("nearbyPlace : " +nearbyPlace.data[0].name);
+                   
+                   
 
              let response = {
                                     attachment: {
@@ -72,26 +74,27 @@ var process = exports.process = function (_Intent) {
                                         payload: {
                                             template_type: "list",
                                             top_element_style: "compact",
-                                            //  elements: [
-                                            //  {
-                                            //     title: "Classic T-Shirt Collection",
-                                            //     subtitle: "See all our colors",
-                                            //     image_url: "https://scontent.xx.fbcdn.net/v/t1.0-1/c1.0.50.50/p50x50/10645025_758268484219520_6811951924776374550_n.jpg?_nc_cat=101&oh=6be0c983f2f5307ccb82caea1204293d&oe=5C29187B",          
-                                            //     buttons: [
-                                            //     {
-                                            //         title: "View",
-                                            //         type: "web_url",
-                                            //         url: "https://peterssendreceiveapp.ngrok.io/collection",
-                                            //         messenger_extensions: true,
-                                            //         webview_height_ratio: "tall",
-                                            //         fallback_url: "https://peterssendreceiveapp.ngrok.io/"            
-                                            //     }
-                                            //     ]
-                                            //  }],
+                                            
+                                             elements: [
+                                             {
+                                                title: "Classic T-Shirt Collection",
+                                                subtitle: "See all our colors",
+                                                image_url: "https://scontent.xx.fbcdn.net/v/t1.0-1/c1.0.50.50/p50x50/10645025_758268484219520_6811951924776374550_n.jpg?_nc_cat=101&oh=6be0c983f2f5307ccb82caea1204293d&oe=5C29187B"     
+                                                // buttons: [
+                                                // {
+                                                //     title: "View",
+                                                //     type: "web_url",
+                                                //     url: "https://peterssendreceiveapp.ngrok.io/collection",
+                                                //     messenger_extensions: true,
+                                                //     webview_height_ratio: "tall",
+                                                //     fallback_url: "https://peterssendreceiveapp.ngrok.io/"            
+                                                // }
+                                                // ]
+                                             }],
                                             buttons: [{
                                                 type: "web_url",
                                                 url: "https://chatbotboom.herokuapp.com/messengerView",
-                                                title: "Set preferences",
+                                                title: "View More",
                                                 webview_height_ratio: "tall",
                                                 messenger_extensions: false
                                             }]
