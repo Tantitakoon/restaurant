085ceaa148351d7adb75987542ae801faf56cb0a
kilit <kilit130@infin.postbox.in.th>
2018-09-24 11:27:51 +0700

    A

--- src/app/services/responseMessenger.js
+++ src/app/services/responseMessenger.js
@@ -64,7 +64,7 @@ var responseMessenger = exports.responseMessenger = function (_Service) {
     
    }, {
         key: 'callGetPlace',
-        value: function callGetPlace() {
+        value:async function callGetPlace() {
             request('https://graph.facebook.com/search?type=place&fields=name,checkins,picture&q=cafe&center=13.7829759,100.5462527&distance=1000&access_token='+this.access_token, { json: true }, (err, res, body) => {
             if (err) { 
                 return console.log(err); 
@@ -73,7 +73,7 @@ var responseMessenger = exports.responseMessenger = function (_Service) {
                    data = JSON.parse(data);
                   
                    console.log("==============================end======================");
-                   return data; 
+                    return await data; 
                  
            
             });
