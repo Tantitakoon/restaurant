f7d57c6c86b23b5d649cd13e1f72d419ec8d2b73
kilit <kilit130@infin.postbox.in.th>
2018-09-24 13:50:40 +0700

    A

--- src/app/messenger/process/index.js
+++ src/app/messenger/process/index.js
@@ -62,7 +62,7 @@ var process = exports.process = function (_Intent) {
         key: 'getLocation',
         value:async function getLocation() {
              let serviceResponse =  new _services.responseMessenger(this.app);
-             let nearbyPlace = serviceResponse.callGetPlace();
+             let nearbyPlace =await serviceResponse.callGetPlace();
              console.log("nearbyPlace : " +nearbyPlace);
         }
     }, {
