7f58ce22436ffa68c97bbbda0f2331439849ab73
kilit <kilit130@infin.postbox.in.th>
2018-09-25 10:03:36 +0700

    A

--- src/app/messenger/process/index.js
+++ src/app/messenger/process/index.js
@@ -66,123 +66,106 @@ var process = exports.process = function (_Intent) {
              let nearbyPlace = await serviceResponse.callGetPlace();
              console.log("nearbyPlace : " +nearbyPlace.data[0].name);
 
-//            var response =  {
+           var response =  {
   
-//     attachment: {
-//       type: "template",
-//       payload: {
-//         template_type: "list",
-//         top_element_style: "compact",
-//         elements: [
-//           {
-//             title: 'title',
-//             image_url: null,
-//             subtitle: 'subtitle subtitle subtitle',
-//             buttons: [
-//               {
-//                 title: 'title',
-//                 type: 'web_url',
-//                 url: 'http://www.google.com.au',
-//                 webview_height_ratio: 'compact'
-//               }
-//             ]
-//           },
-//           {
-//             title: 'title',
-//             image_url: null,
-//             subtitle: 'subtitle subtitle subtitle',
-//             buttons: [
-//               {
-//                 title: 'title',
-//                 type: 'web_url',
-//                 url: 'http://www.google.com.au',
-//                 webview_height_ratio: 'compact'
-//               }
-//             ]
-//           }
-//         ],"buttons": [
-//           {
-//             "title": "View More",
-//             "type": "postback",
-//             "payload": "payload"            
-//           }
-//         ]  
-//       }
-//     }
-  
-// }
-
-
-
-
-var response = {
- 
-  
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "list",
-       
-        "elements": [
-          {
-            "title": "Classic T-Shirt Collection",
-            "subtitle": "See all our colors",
-            "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
-            "buttons": [
-              {
-                "title": "View",
-                "type": "web_url",
-                "url": "https://peterssendreceiveapp.ngrok.io/collection",
-                "messenger_extensions": true,
-                "webview_height_ratio": "tall",
-                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-              }
-            ]
-          },
-          {
-            "title": "Classic White T-Shirt",
-            "subtitle": "See all our colors",
-            "default_action": {
-              "type": "web_url",
-              "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-              "messenger_extensions": false,
-              "webview_height_ratio": "tall"
-            }
-          },
-          {
-            "title": "Classic Blue T-Shirt",
-            "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-            "subtitle": "100% Cotton, 200% Comfortable",
-            "default_action": {
-              "type": "web_url",
-              "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-              "messenger_extensions": true,
-              "webview_height_ratio": "tall",
-              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-            },
-            "buttons": [
-              {
-                "title": "Shop Now",
-                "type": "web_url",
-                "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                "messenger_extensions": true,
-                "webview_height_ratio": "tall",
-                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-              }
-            ]        
-          }
-        ],
-         "buttons": [
-          {
-            "title": "View More",
-            "type": "postback",
-            "payload": "payload"            
-          }
-        ]  
-      }
-    }
-  
+                attachment: {
+                type: "template",
+                payload: {
+                    template_type: "list",
+                    top_element_style: "compact",
+                    elements: [
+                    {
+                        title: 'title',
+                        image_url: null,
+                        subtitle: 'subtitle subtitle subtitle',
+                        buttons: [
+                        {
+                            title: 'title',
+                            type: 'web_url',
+                            url: 'http://www.google.com.au',
+                            webview_height_ratio: 'compact'
+                        }
+                        ]
+                    },
+                    {
+                        title: 'title',
+                        image_url: null,
+                        subtitle: 'subtitle subtitle subtitle',
+                        buttons: [
+                        {
+                            title: 'title',
+                            type: 'web_url',
+                            url: 'http://www.google.com.au',
+                            webview_height_ratio: 'compact'
+                        }
+                        ]
+                    },
+                    {
+                        title: 'title',
+                        image_url: null,
+                        subtitle: 'subtitle subtitle subtitle',
+                        buttons: [
+                        {
+                            title: 'title',
+                            type: 'web_url',
+                            url: 'http://www.google.com.au',
+                            webview_height_ratio: 'compact'
+                        }
+                        ]
+                    },
+                    {
+                        title: 'title',
+                        image_url: null,
+                        subtitle: 'subtitle subtitle subtitle',
+                        buttons: [
+                        {
+                            title: 'title',
+                            type: 'web_url',
+                            url: 'http://www.google.com.au',
+                            webview_height_ratio: 'compact'
+                        }
+                        ]
+                    },
+                    {
+                        title: 'title',
+                        image_url: null,
+                        subtitle: 'subtitle subtitle subtitle',
+                        buttons: [
+                        {
+                            title: 'title',
+                            type: 'web_url',
+                            url: 'http://www.google.com.au',
+                            webview_height_ratio: 'compact'
+                        }
+                        ]
+                    },
+                    {
+                        title: 'title',
+                        image_url: null,
+                        subtitle: 'subtitle subtitle subtitle',
+                        buttons: [
+                        {
+                            title: 'title',
+                            type: 'web_url',
+                            url: 'http://www.google.com.au',
+                            webview_height_ratio: 'compact'
+                        }
+                        ]
+                    }
+                    ],"buttons": [
+                    {
+                        "title": "View More",
+                        "type": "postback",
+                        "payload": "payload"            
+                    }
+                    ]  
+                }
+                }
+            
 }
+
+
+
                  
                 let request_body = {
                     "recipient": {
